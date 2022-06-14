d22566ffdbde114daf1a1f5a094741091ff430c4
Leonard Heilos <leonard.heilos@sap.com>
2022-06-14 15:44:43 +0200

    Allow to create from VCAP environment variables

--- src/main/java/com/sap/mobile/services/client/push/MobileServicesBinding.java
+++ src/main/java/com/sap/mobile/services/client/push/MobileServicesBinding.java
@@ -3,21 +3,34 @@ package com.sap.mobile.services.client.push;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Slf4j
 public final class MobileServicesBinding {
+
+	private static final String VCAP_SERVICES_NAME = "VCAP_SERVICES";
+	private static final String MOBILE_SERVICES_TAG_PREFIX = "mobileservice";
 
 	public static MobileServicesBinding fromResource(String resource) throws IOException {
 		try (InputStream is = MobileServicesBinding.class.getClassLoader().getResourceAsStream(resource)) {
@@ -27,6 +40,39 @@ public final class MobileServicesBinding {
 
 	public static MobileServicesBinding fromInputStream(InputStream stream) throws IOException {
 		return new ObjectMapper().readValue(stream, MobileServicesBinding.class);
+	}
+
+	public static Optional<MobileServicesBinding> fromVCAPVariables() throws IOException {
+		return fromVCAPVariables(System::getenv);
+	}
+
+	static Optional<MobileServicesBinding> fromVCAPVariables(final Function<String, String> environmentAccessor) throws IOException {
+		final String vcapServices = environmentAccessor.apply(VCAP_SERVICES_NAME);
+		if (vcapServices == null) {
+			log.debug("No '{}' environment variable found", VCAP_SERVICES_NAME);
+			return Optional.empty();
+		}
+
+		log.debug("Parsing bound services");
+		final ObjectMapper objectMapper = new ObjectMapper();
+		final VcapServices parsedService = objectMapper.readValue(vcapServices, VcapServices.class);
+		final List<VcapService> serviceInstances = parsedService.values()
+				.stream()
+				.flatMap(List::stream)
+				.filter(service -> service.tags.stream().anyMatch(tag -> tag.startsWith(MOBILE_SERVICES_TAG_PREFIX)))
+				.collect(Collectors.toList());
+
+		if (serviceInstances.isEmpty()) {
+			log.info("No Mobile Services binding found");
+			return Optional.empty();
+		} else if (serviceInstances.size() > 1) {
+			final String foundServiceNames = serviceInstances.stream().map(VcapService::getName).collect(Collectors.joining(", "));
+			log.warn("Found multiple service bindings: {}", foundServiceNames);
+			return Optional.empty();
+		}
+
+		final VcapService service = serviceInstances.get(0);
+		return Optional.of(objectMapper.convertValue(service.getCredentials(), MobileServicesBinding.class));
 	}
 
 	@JsonProperty("endpoints")
@@ -61,5 +107,21 @@ public final class MobileServicesBinding {
 		private String url;
 		@JsonProperty("identityzone")
 		private String identityZone;
+	}
+
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	private static class VcapServices extends HashMap<String, List<VcapService>> {
+	}
+
+	@Getter
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	private static class VcapService {
+		@JsonProperty("name")
+		private String name;
+		@JsonProperty("tags")
+		@JsonInclude(JsonInclude.Include.NON_NULL)
+		private Set<String> tags = new HashSet<>();
+		@JsonProperty("credentials")
+		private ObjectNode credentials;
 	}
 }
--- src/test/java/com/sap/mobile/services/client/push/MobileServicesBindingTest.java
+++ src/test/java/com/sap/mobile/services/client/push/MobileServicesBindingTest.java
@@ -4,9 +4,26 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.is;
 
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MobileServicesBindingTest {
+
+	private Map<String, String> envVariables;
+	private Function<String, String> environmentAccessor;
+
+	@Before
+	public void setUp() {
+		envVariables = new HashMap<>();
+		environmentAccessor = envVariables::get;
+	}
 
 	@Test
 	public void testBinding() throws Exception {
@@ -22,5 +39,51 @@ public class MobileServicesBindingTest {
 		assertThat(settings.getClientConfiguration().getIdentityZone(), is("identityZone"));
 		assertThat(settings.getClientConfiguration().getUrl(),
 				is("uaaUrl"));
+	}
+
+	@Test
+	public void testBindingVcapEmpty() throws Exception {
+		String vcapServices = IOUtils.resourceToString("/vcap/vcap-services-empty.json", StandardCharsets.UTF_8);
+		envVariables.put("VCAP_SERVICES", vcapServices);
+
+		assertThat(MobileServicesBinding.fromVCAPVariables(environmentAccessor), is(Optional.empty()));
+	}
+
+	@Test
+	public void testBindingVcapNoMobileServices() throws Exception {
+		String vcapServices = IOUtils.resourceToString("/vcap/vcap-services-no-mobile-services.json", StandardCharsets.UTF_8);
+		envVariables.put("VCAP_SERVICES", vcapServices);
+
+		assertThat(MobileServicesBinding.fromVCAPVariables(environmentAccessor), is(Optional.empty()));
+	}
+
+	@Test
+	public void testBindingVcapSingleMatch() throws Exception {
+		String vcapServices = IOUtils.resourceToString("/vcap/vcap-services-single-match.json", StandardCharsets.UTF_8);
+		envVariables.put("VCAP_SERVICES", vcapServices);
+
+		Optional<MobileServicesBinding> result = MobileServicesBinding.fromVCAPVariables(environmentAccessor);
+		assertThat(result.isPresent(), is(true));
+
+		MobileServicesBinding binding = result.get();
+		assertThat(binding.getAppName(), is("appName"));
+		assertThat(binding.getEndpoints(), hasKey("mobileservices"));
+		assertThat(binding.getEndpoints().get("mobileservices").getTimeout(), is(160000));
+		assertThat(binding.getEndpoints().get("mobileservices").getUrl(),
+				is("https://anyUrl"));
+		assertThat(binding.getClientConfiguration().getClientId(),
+				is("clientId"));
+		assertThat(binding.getClientConfiguration().getClientSecret(), is("clientSecret"));
+		assertThat(binding.getClientConfiguration().getIdentityZone(), is("identityZone"));
+		assertThat(binding.getClientConfiguration().getUrl(),
+				is("uaaUrl"));
+	}
+
+	@Test
+	public void testBindingVcapMultipleMatches() throws Exception {
+		String vcapServices = IOUtils.resourceToString("/vcap/vcap-services-multiple-matches.json", StandardCharsets.UTF_8);
+		envVariables.put("VCAP_SERVICES", vcapServices);
+
+		assertThat(MobileServicesBinding.fromVCAPVariables(environmentAccessor), is(Optional.empty()));
 	}
 }
