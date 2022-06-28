c0b1281b446243c929d2472da82806563816d999
Leonard Heilos <leonard.heilos@sap.com>
2022-06-28 10:11:59 +0200

    Wrap custom actions in lerna module

---
+++ .github/actions/scripts/build.js
@@ -0,0 +1,60 @@
+const fs = require("fs").promises;
+const path = require("path");
+const yaml = require("js-yaml");
+const ncc = require("@vercel/ncc");
+
+async function build() {
+  try {
+    console.log("Build javascript action module");
+
+    const actionsDescriptorPath = path.join(process.cwd(), "action.yml");
+    const actionsDescriptorString = await fs.readFile(actionsDescriptorPath, {
+      encoding: "utf-8",
+    });
+    const actionsDescriptor = yaml.load(actionsDescriptorString);
+
+    const main = actionsDescriptor.runs.main;
+    const post = actionsDescriptor.runs.post;
+
+    if (main) {
+      await compile(main);
+    }
+
+    if (post) {
+      await compile(post);
+    }
+
+    process.exit(0);
+  } catch (e) {
+    console.error(e);
+    process.exit(-1);
+  }
+}
+
+async function compile(name) {
+  const sourceFile = path.join(process.cwd(), name);
+  const distFolder = path.join(process.cwd(), "dist");
+  const outFile = path.join(distFolder, name);
+  const backupSourceFile = path.join(distFolder, name + ".orig");
+
+  console.log(`Compile ${sourceFile}`);
+  const result = await ncc(sourceFile);
+
+  try {
+    await fs.rm(distFolder, {
+      recursive: true,
+      force: true,
+    });
+  } catch (e) {
+    //ignore
+  }
+
+  await fs.mkdir(distFolder);
+
+  await fs.copyFile(sourceFile, backupSourceFile);
+  await fs.writeFile(outFile, result.code, {
+    encoding: "utf-8",
+  });
+}
+
+build();
