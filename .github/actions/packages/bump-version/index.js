fc65ef0d618a8ff4b908f96376c688e18237bf07
Leonard Heilos <leonard.heilos@sap.com>
2022-07-01 12:45:43 +0200

    Update bump-version action

--- .github/actions/packages/bump-version/index.js
+++ .github/actions/packages/bump-version/index.js
@@ -6,18 +6,23 @@ try {
 
     const currentVersion = core.getInput('current-version');
     const type = core.getInput('type');
+    const isDev = (core.getInput('is-dev') === 'true');
 
     const parsedVersion = semver.parseSemVer(currentVersion, true);
     const currentBranch = toBranch(parsedVersion);
+    let suffix = isDev ? '-dev' : '';
     let targetVersion;
     let targetBranch;
+    let targetTag;
 
     if (type === 'major') {
-        targetVersion = `${parsedVersion.major + 1}.0.0`;
+        targetVersion = `${parsedVersion.major + 1}.0.0${suffix}`;
     } else if (type === 'minor') {
-        targetVersion = `${parsedVersion.major}.${parsedVersion.minor + 1}.0`;
+        targetVersion = `${parsedVersion.major}.${parsedVersion.minor + 1}.0${suffix}`;
     } else if (type === 'patch') {
-        targetVersion = `${parsedVersion.major}.${parsedVersion.minor}.${parsedVersion.patch + 1}`;
+        targetVersion = `${parsedVersion.major}.${parsedVersion.minor}.${parsedVersion.patch + 1}${suffix}`;
+    } else if (type === 'release') {
+        targetVersion = `${parsedVersion.major}.${parsedVersion.minor}.${parsedVersion.patch}${suffix}`;
     } else if (type === 'run') {
         const commitId = core.getInput('commit-id');
         if (commitId) {
@@ -28,15 +33,21 @@ try {
     }
 
     targetBranch = toBranch(semver.parseSemVer(targetVersion, true));
+    targetTag = toTag(semver.parseSemVer(targetVersion, true));
 
     core.setOutput("current-version", currentVersion);
     core.setOutput("current-branch", currentBranch);
     core.setOutput("target-version", targetVersion);
     core.setOutput("target-branch", targetBranch);
+    core.setOutput("target-tag", targetTag);
 } catch (error) {
     core.setFailed(error.message);
 }
 
 function toBranch(version) {
     return `rel/${version.major}.${version.minor}.x`;
+}
+
+function toTag(version) {
+    return `v${version.major}.${version.minor}.${version.patch}`;
 }
