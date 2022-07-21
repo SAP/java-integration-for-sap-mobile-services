0628175759848611fe5cf2f972cefc50fbc225f7
Leonard Heilos <leonard.heilos@sap.com>
2022-07-21 15:01:02 +0200

    Support to generate settings-file in pipeline

--- .github/actions/packages/prepare-mobile-services-app/index.js
+++ .github/actions/packages/prepare-mobile-services-app/index.js
@@ -16,22 +16,37 @@ async function run() {
         const mobileFeatures = core.getInput("mobile-features");
         const parsedMobileFeatures = mobileFeatures.split(',').filter(f => f.length > 0);
         const serviceKeyPath = core.getInput('service-key-path');
+        const configurations = JSON.parse(core.getInput('configurations') || '[]');
 
-        const mobileAppConfig = await client.createMobileApplication({
+        const clientConfig = {
             brokerEndpoint: brokerEndpoint,
             xsuaaClientId: xsuaaClientId,
             xsuaaUrl: xsuaaUrl,
             xsuaaCert: xsuaaCert,
             xsuaaKey: xsuaaKey
-        }, parsedMobileFeatures);
+        };
 
-        await fs.writeFile('.mobile_app_id', mobileAppConfig['sap.cloud.service'], {
+        const mobileAppConfig = await client.createMobileApplication(clientConfig, parsedMobileFeatures);
+        const appId = mobileAppConfig['sap.cloud.service'];
+        await fs.writeFile('.mobile_app_id', appId, {
             encoding: 'utf-8'
         });
 
         const serviceKeyJsonString = JSON.stringify(mobileAppConfig)
         if (serviceKeyPath) {
             await fs.writeFile(serviceKeyPath, serviceKeyJsonString, {
+                encoding: 'utf-8'
+            });
+        }
+
+        for (const configOption of configurations) {
+            const config = await client.createMobileServicesConfiguration(clientConfig, appId, {
+                serviceName: configOption.serviceName,
+                scopes: configOption.scopes,
+                x509: configOption.x509 === true
+            });
+            const configJsonString = JSON.stringify(config);
+            await fs.writeFile(configOption.configPath, configJsonString, {
                 encoding: 'utf-8'
             });
         }
